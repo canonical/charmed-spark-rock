ad59e72502d5de4f95b9bce5b50cca152be3957b
Enrico Deusebio <enrico.deusebio@canonical.com>
2023-06-21 17:42:33 +0200

    using case statement and providing infinite loop capabilities

--- files/spark/bin/charmed-spark-entrypoint.sh
+++ files/spark/bin/charmed-spark-entrypoint.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 TYPE=$1
-shift
 
 echo "Running script with ${TYPE} flavour"
 
-if [ "${TYPE}" == "history-server" ]; then
-  cd /opt/spark
-  ./sbin/start-history-server.sh --properties-file ${SPARK_PROPERTIES_FILE}
-elif [ "${TYPE}" == "jobs" ]; then
-  cd /opt/spark
-  ./entrypoint.sh "$*"
-else
-  echo "Component \"${TYPE}\" unknown"
-  exit 1
-fi
+case "${TYPE}" in
+  driver|executor)
+    cd /opt/spark
+    ./entrypoint.sh "$*"
+    ;;
+  history-server)
+    cd /opt/spark
+    ./sbin/start-history-server.sh --properties-file ${SPARK_PROPERTIES_FILE}
+    ;;
+  "")
+    # Infinite loop to allow pebble to be running indefinitely
+    while true; do sleep 5; done
+    ;;
+  *)
+    echo "Component \"${TYPE}\" unknown"
+    exit 1
+    ;;
+esac
