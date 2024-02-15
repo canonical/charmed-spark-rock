e6638b34863c94da39f65a744c4f1347fce181d6
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2024-02-15 17:10:06 +0545

    Add Spark SQL to spark rock image

--- tests/integration/resources/test-spark-sql.sql
+++ tests/integration/resources/test-spark-sql.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE IF NOT EXISTS sparkdb;
 USE sparkdb;
-CREATE TABLE sparkdb.testTable (number Int, word String);
+CREATE TABLE IF NOT EXISTS sparkdb.testTable (number Int, word String);
 INSERT INTO sparkdb.testTable VALUES (1, "foo"), (2, "bar"), (3, "grok");
-SELECT "Inserted Rows:", COUNT(*) FROM sparkdb.testTable;
+SELECT CONCAT("Inserted Rows: ", COUNT(*)) FROM sparkdb.testTable;
+EXIT;
