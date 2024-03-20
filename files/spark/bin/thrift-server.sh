a1dc8219c6a6c8dc89814ba98d55ece44e3338e3
bikalpa <bikalpa.dhakal@canonical.com>
2024-03-20 17:58:01 +0545

    [DPE-3835] Remove `kyuubi-env.sh` file from Charmed Spark Rock image (#78)

--- files/spark/bin/thrift-server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pushd /opt/spark
-/opt/spark/sbin/start-thriftserver.sh --properties-file ${SPARK_PROPERTIES_FILE}
--- files/spark/conf/kyuubi-env.sh
+++
@@ -1,3 +0,0 @@
-export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
-export SPARK_HOME="/opt/spark/"
-export SPARK_CONF_DIR="/etc/spark8t/conf"
