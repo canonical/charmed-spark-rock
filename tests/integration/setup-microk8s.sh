017c69753650ddfd1e20c127edbd65b565c6f962
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-04-12 13:36:58 +0000

    Test microk8s 1.29 strict

--- tests/integration/setup-microk8s.sh
+++ tests/integration/setup-microk8s.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo snap install microk8s --channel=1.28/stable --classic
+sudo snap install microk8s --channel=1.29-strict/stable --classic
 sudo snap alias microk8s.kubectl kubectl
 sudo usermod -a -G microk8s ${USER}
 mkdir -p ~/.kube
