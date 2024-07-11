a261c3ea684186c57546d6cf64c2435a6216455f
welpaolo <paolo.sottovia@canonical.com>
2024-07-11 18:32:47 +0200

    [DPE-4629] Update Spark GPU image to Java17 (#95)

--- tests/integration/integration-tests-gpu.sh
+++ tests/integration/integration-tests-gpu.sh
@@ -190,8 +190,123 @@ run_test_gpu_example_in_pod(){
 
 }
 
-test_test_gpu_example_in_pod() {
+test_gpu_example_in_pod() {
   run_test_gpu_example_in_pod $NAMESPACE spark
+}
+
+
+teardown_test_pod() {
+  kubectl logs testpod-admin -n $NAMESPACE 
+  kubectl logs testpod -n $NAMESPACE 
+  kubectl logs -l spark-version=3.4.2 -n $NAMESPACE 
+  kubectl -n $NAMESPACE delete pod testpod
+  kubectl -n $NAMESPACE delete pod testpod-admin
+
+  kubectl delete namespace $NAMESPACE
+}
+
+
+run_test_sql_gpu_example_in_pod(){
+  # Test Spark-rapids integration in Charmed Spark Rock
+
+  # First create S3 bucket named 'spark'
+  create_s3_bucket $S3_BUCKET
+
+  # Copy 'test-iceberg.py' script to 'spark' bucket
+  copy_file_to_s3_bucket $S3_BUCKET ./tests/integration/resources/test-gpu-sql.py
+
+  # create data bucket
+
+  create_s3_bucket "data"
+
+  # copy test data
+  aws s3 cp ./tests/integration/resources/tpcds/catalog_sales/catalog_sales.parquet s3://data/tpcds/catalog_sales/catalog_sales.parquet
+  aws s3 cp ./tests/integration/resources/tpcds/date_dim/date_dim.parquet s3://data/tpcds/date_dim/date_dim.parquet
+  aws s3 cp ./tests/integration/resources/tpcds/item/item.parquet s3://data/tpcds/item/item.parquet
+  aws s3 cp ./tests/integration/resources/tpcds/store_sales/store_sales.parquet s3://data/tpcds/store_sales/store_sales.parquet
+  aws s3 cp ./tests/integration/resources/tpcds/web_sales/web_sales.parquet s3://data/tpcds/web_sales/web_sales.parquet
+  aws s3 cp ./tests/integration/resources/tpcds/customer/customer.parquet s3://data/tpcds/customer/customer.parquet
+
+  # Number of driver pods that exist in the namespace already.
+  PREVIOUS_DRIVER_PODS_COUNT=$(kubectl get pods --sort-by=.metadata.creationTimestamp -n ${NAMESPACE} | grep driver | wc -l)
+
+  # Submit the job from inside 'testpod'
+  kubectl -n $NAMESPACE exec testpod -- \
+      env \
+        UU="$SERVICE_ACCOUNT" \
+        NN="$NAMESPACE" \
+        ACCESS_KEY="$(get_s3_access_key)" \
+        SECRET_KEY="$(get_s3_secret_key)" \
+        S3_ENDPOINT="$(get_s3_endpoint)" \
+        BUCKET="$S3_BUCKET" \
+        IM="$(spark_image)" \
+      /bin/bash -c '\
+        spark-client.spark-submit \
+        --username $UU \
+        --namespace $NN \
+        --conf spark.kubernetes.driver.request.cores=100m \
+        --conf spark.kubernetes.executor.request.cores=100m \
+        --conf spark.hadoop.fs.s3a.aws.credentials.provider=org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider \
+        --conf spark.hadoop.fs.s3a.connection.ssl.enabled=false \
+        --conf spark.hadoop.fs.s3a.path.style.access=true \
+        --conf spark.hadoop.fs.s3a.endpoint=$S3_ENDPOINT \
+        --conf spark.hadoop.fs.s3a.access.key=$ACCESS_KEY \
+        --conf spark.hadoop.fs.s3a.secret.key=$SECRET_KEY \
+        --conf spark.executor.instances=1 \
+        --conf spark.executor.resource.gpu.amount=1 \
+        --conf spark.executor.memory=4G \
+        --conf spark.executor.cores=1 \
+        --conf spark.task.cpus=1 \
+        --conf spark.task.resource.gpu.amount=1 \
+        --conf spark.rapids.memory.pinnedPool.size=1G \
+        --conf spark.executor.memoryOverhead=1G \
+        --conf spark.sql.files.maxPartitionBytes=512m \
+        --conf spark.sql.shuffle.partitions=10 \
+        --conf spark.plugins=com.nvidia.spark.SQLPlugin \
+        --conf spark.executor.resource.gpu.discoveryScript=/opt/getGpusResources.sh \
+        --conf spark.executor.resource.gpu.vendor=nvidia.com \
+        --conf spark.kubernetes.container.image=$IM \
+        --driver-memory 2G \
+        --conf spark.kubernetes.executor.podTemplateFile=/etc/spark/conf/gpu_executor_template.yaml \
+        --conf spark.kubernetes.executor.deleteOnTermination=false \
+          s3a://$BUCKET/test-gpu-sql.py'
+
+  # Delete 'spark' bucket
+  delete_s3_bucket $S3_BUCKET
+
+  # Delete 'data' bucket
+  delete_s3_bucket "data"
+
+  # Number of driver pods after the job is completed.
+  DRIVER_PODS_COUNT=$(kubectl get pods --sort-by=.metadata.creationTimestamp -n ${NAMESPACE} | grep driver | wc -l)
+
+  # If the number of driver pods is same as before, job has not been run at all!
+  if [[ "${PREVIOUS_DRIVER_PODS_COUNT}" == "${DRIVER_PODS_COUNT}" ]]
+  then
+    echo "ERROR: Sample job has not run!"
+    exit 1
+  fi
+
+  # Find the ID of the driver pod that ran the job.
+  # tail -n 1       => Filter out the last line
+  # cut -d' ' -f1   => Split by spaces and pick the first part
+  DRIVER_POD_ID=$(kubectl get pods --sort-by=.metadata.creationTimestamp -n ${NAMESPACE} | grep driver | tail -n 1 | cut -d' ' -f1)
+
+  # Filter out the output log line
+  OUTPUT_LOG_LINE=$(kubectl logs ${DRIVER_POD_ID} -n ${NAMESPACE} | grep "average")
+  echo "output log line: $OUTPUT_LOG_LINE"
+  # Fetch out the number of rows with the desired keyword
+  NUM_ROWS=$(echo $OUTPUT_LOG_LINE | wc -l)
+  echo "number of rows: $NUM_ROWS"
+  if [ "${NUM_ROWS}" == 0 ]; then
+      echo "ERROR: No GPU enable workflow found. Aborting with exit code 1."
+      exit 1
+  fi
+
+}
+
+test_sql_gpu_example_in_pod() {
+  run_test_sql_gpu_example_in_pod $NAMESPACE spark
 }
 
 cleanup_user_failure_in_pod() {
@@ -211,7 +326,15 @@ echo -e "##################################"
 echo -e "RUN EXAMPLE THAT USES GPU"
 echo -e "##################################"
 
-(setup_user_context && test_test_gpu_example_in_pod && cleanup_user_success) || cleanup_user_failure_in_pod
+(setup_user_context && test_gpu_example_in_pod && cleanup_user_success) || cleanup_user_failure_in_pod
+
+
+echo -e "##################################"
+echo -e "RUN SQL EXAMPLE THAT USES GPU"
+echo -e "##################################"
+
+(setup_user_context && test_sql_gpu_example_in_pod && cleanup_user_success) || cleanup_user_failure_in_pod
+
 
 echo -e "##################################"
 echo -e "TEARDOWN TEST POD"
