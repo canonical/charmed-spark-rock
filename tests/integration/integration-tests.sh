0077bbd1b9e2807e3d2b99ae31ac4f8564ed38ca
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-08-19 14:59:23 +0000

    wip

--- tests/integration/integration-tests.sh
+++ tests/integration/integration-tests.sh
@@ -148,6 +148,14 @@ run_example_job_in_pod() {
 
   logs=$(kubectl logs $(kubectl get pods --sort-by=.metadata.creationTimestamp -n ${NAMESPACE} | grep driver | tail -n 1 | cut -d' ' -f1)  -n ${NAMESPACE})
   echo "logs: $logs"
+
+  lines=$(echo $logs | grep 'task 0.0' | wc -l)
+  
+  if [ "$lines" -eq 0 ]; then
+    echo "Zero lines in the logs... something wrong"
+    exit 1
+  fi
+
   # Check job output
   # Sample output
   # "Pi is roughly 3.13956232343"
