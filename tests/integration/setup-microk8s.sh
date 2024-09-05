8429058c1990ebd2882c752091ed948aaec2b8ec
Enrico Deusebio <enrico.deusebio@canonical.com>
2024-09-05 23:07:45 +0200

    pin to strict K8s

--- tests/integration/setup-microk8s.sh
+++ tests/integration/setup-microk8s.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo snap install microk8s --channel=$1 --classic
+sudo snap install microk8s --channel=$1
 sudo snap alias microk8s.kubectl kubectl
-sudo usermod -a -G microk8s ${USER}
+sudo usermod -a -G snap_microk8s ${USER}
 mkdir -p ~/.kube
 sudo chown -f -R ${USER} ~/.kube
