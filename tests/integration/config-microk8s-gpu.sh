493435c5e3073c0c773ce1f7c0b3aa9947958ccb
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-09-06 09:10:33 +0000

    More debug logs

--- tests/integration/config-microk8s-gpu.sh
+++ tests/integration/config-microk8s-gpu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # This needs to be a separate file as newgrp will not have effect from setup_environment.sh
-
+set -x 
 echo "Using addons: $MICROK8S_ADDONS"
-sudo microk8s enable storage $MICROK8S_ADDONS
+sudo microk8s enable $MICROK8S_ADDONS
 sudo microk8s kubectl rollout status deployment/hostpath-provisioner -n kube-system
 
 # Wait for gpu operator components to be ready
@@ -24,3 +24,4 @@ done
 sudo microk8s config > ~/.kube/config
 sudo chown ubuntu:ubuntu ~/.kube/config
 sudo chmod 600 ~/.kube/config
+set +x
