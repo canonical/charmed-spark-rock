90b83d2ce7e2c336dcec4f0394e3cbd31f2293de
Enrico Deusebio <enrico.deusebio@canonical.com>
2024-12-06 08:47:40 +0100

    [DPE-6154] Update rockcraft.yaml recipes using newer Rockcraft features

--- images/charmed-spark/bin/sparkd.sh
+++ images/charmed-spark/bin/sparkd.sh
@@ -24,15 +24,6 @@ function log_forwarding {
   fi
 }
 
-function finish {
-  if [ $? -ne 0 ]
-  then
-    kill -1 1
-    sleep 1
-  fi
-}
-trap finish EXIT
-
 FLAVOUR=$1
 
 echo "Running script with ${FLAVOUR} flavour"
