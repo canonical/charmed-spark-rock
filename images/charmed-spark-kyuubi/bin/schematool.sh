a91b66eb46f1414b3d68a01ca12ab3b3eec4f674
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2025-04-07 13:20:32 +0545

    Add schematool

--- images/charmed-spark-kyuubi/bin/schematool.sh
+++ images/charmed-spark-kyuubi/bin/schematool.sh
@@ -3,11 +3,13 @@
 SPARK_JARS=/opt/spark/jars
 KYUUBI_JARS=/opt/kyuubi/jars
 
-check_jar() {
-  local jar_name=$1
 
-  if [ ! -e "${SPARK_JARS}/${jar_name}" ] && [ ! -e "${KYUUBI_JARS}/${jar_name}" ]; then
-    echo "Missing Jar: ${jar_name}"
+check_jar() {
+  local jar_glob=$1
+
+  if ! compgen -G "${SPARK_JARS}/${jar_glob}" > /dev/null && \
+     ! compgen -G "${KYUUBI_JARS}/${jar_glob}" > /dev/null; then
+    echo "Missing Jar: ${jar_glob}"
     exit 1
   fi
 }
@@ -18,6 +20,7 @@ check_jar "hive-metastore-*.jar"
 check_jar "hive-cli-*.jar"
 
 export HIVE_HOME=/opt/hive
+export HIVE_CONF_DIR=$SPARK_CONF_DIR
 CLASSPATH="${SPARK_JARS}/*:${KYUUBI_JARS}/*"
 
 CLASS=org.apache.hive.beeline.HiveSchemaTool
